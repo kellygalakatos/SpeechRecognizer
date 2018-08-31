b9aa08d13d0224fda0f3b24fa5ca923af5b14636
Kelly Galakatos <kellygalakatos@2600-6c40-4b00-0e1c-14d1-461c-72df-80f3.dhcp6.chtrptr.net>
2018-08-31 18:51:08 -0500

    Commit project

--- SpeechRecognizer/ViewController.swift
+++ SpeechRecognizer/ViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
+    @IBOutlet weak var textLabel: UILabel!
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        
+        guard let myRecognizer = SFSpeechRecognizer() else { return }
+        if !myRecognizer.isAvailable { return }
+        
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: {result, error in
+            
+            if let result = result {
+                let bestString = result.bestTranscription.formattedString
+                self.textLabel.text = bestString
+            } else if let error = error {
+                print(error)
+            }
+        })
+        
+    }
 
-
+    @IBAction func startButtonClicked(_ sender: Any) {
+        self.recordAndRecognizeSpeech()
+    }
+    
 }
 
